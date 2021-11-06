043c238e0a816261bd79bc25bdf9d18906138da7
jiseong12 <>
2021-11-06 01:44:40 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+function HomePage() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <h1 style={{ fontFamily: 'Cursive', color: '#2b00ff' }}>
+        {`cars's website`}
+      </h1>
+      <h2
+        style={{
+          fontFamily: 'cursive',
+          color: 'rgb(243, 103, 123)',
+          marginBottom: '10rem'
+        }}
+      >
+        click the buttons below
+      </h2>
+      <button
+        style={{
+          fontFamily: 'monospace',
+          padding: '1rem',
+          fontSize: '2rem',
+          background: 'blue',
+          color: 'white'
+        }}
+        onClick={() => alert('I like cars!')}
+      >
+        Welcome
+      </button>
+      <button
+        style={{
+          fontFamily: 'monospace',
+          padding: '1rem',
+          fontSize: '2rem',
+          background: 'orange',
+          color: 'white',
+          marginTop: '2rem'
+        }}
+        onClick={() => {
+          let name = prompt("What's your name?");
+          if (name) {
+            alert('Nice to meet you, ' + name + '!');
+          } else {
+            alert('do you like cars?');
+          }
+        }}
+      >
+        {"What's your name?"}
+      </button>
+    </div>
+  );
+}
+
+export default HomePage;
